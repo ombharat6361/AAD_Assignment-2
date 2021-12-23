febaa7c4eeac0e0102f34ba81534c96d5acbd9f5
ombharat6361 <96538649+ombharat6361@users.noreply.github.com>
2021-12-23 19:08:43 +0530

    Update Reverse number.java

--- Reverse number.java
+++ Reverse number.java
@@ -1,14 +1,14 @@
 package com.company;
-import java.util.Scanner;
+import java.util.Scanner; //for taking user input
 public class Main {
 
     public static void main(String[] args)
     {
         System.out.println("Enter number: ");
         Scanner num=new Scanner(System.in);
-        long n = num.nextLong();
+        long n = num.nextLong();//using long to factor in larger numbers
         long temp=n,r,sum=0;
-
+        //looping to extract digits and reverse the number
         while(n>0)
         {
             r=n%10;
