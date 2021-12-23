ccfa3f2d8ef603a76651a0e62b25a95edcd3f16f
ombharat6361 <96538649+ombharat6361@users.noreply.github.com>
2021-12-23 15:18:58 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Enter number: ");
+        Scanner num=new Scanner(System.in);
+        long n = num.nextLong();
+        long temp=n,r,sum=0;
+
+        while(n>0)
+        {
+            r=n%10;
+            sum=sum*10+r;
+            n=n/10;
+        }
+        System.out.println(sum);
+    }
+}
