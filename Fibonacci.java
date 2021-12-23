97c4f080d564a7d2a5c305bb57113c35f8be000d
ombharat6361 <96538649+ombharat6361@users.noreply.github.com>
2021-12-23 19:04:40 +0530

    Java Basic programs

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,20 +1,20 @@
 package com.company;
-import java.util.Scanner;
+import java.util.Scanner; //for taking user input
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Enter how long you want the Fibonacci series to be: ");
         Scanner num= new Scanner(System.in);
         int n = num.nextInt();
-	    int t1=0,t2=1, Fsum=1,e;
+	int t1=0,t2=1, Fsum=1,e;
         System.out.println(t1);
         System.out.println(Fsum);
-        for(int i=2;i<n;i++)
+        for(int i=2;i<n;i++) //looping for the series
         {
             Fsum=t2+t1;
             System.out.println(Fsum);
-            t1=t2;
-            t2=Fsum;
+            t1=t2;     //storing old value of t2 in t1 to add in the next loop
+            t2=Fsum;   //Similarly, storing old value of Fsum (newest value) in t2 to add in the next loop
         }
     }
 }
