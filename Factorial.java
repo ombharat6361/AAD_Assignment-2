8e3b5cac0b26b7a6f5d4ff1b55e1f90e090a90c6
ombharat6361 <96538649+ombharat6361@users.noreply.github.com>
2021-12-23 19:01:49 +0530

    Java Basic programs

--- Factorial.java
+++ Factorial.java
@@ -1,6 +1,6 @@
 package com.company;
 
-import java.util.Scanner;
+import java.util.Scanner; //for taking user input
 
 public class Main {
 
@@ -9,6 +9,7 @@ public class Main {
         Scanner num = new Scanner(System.in);
         long n = num.nextLong();
         long Fac=1;
+        //looping to find factorial by extracting digits
         while(n>0)
         {
             Fac=Fac*n;
