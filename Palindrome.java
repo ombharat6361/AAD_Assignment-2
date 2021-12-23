21d097f181c0d1272000cd732b0fd8c9b05bc7d4
ombharat6361 <96538649+ombharat6361@users.noreply.github.com>
2021-12-23 19:07:07 +0530

    Java Basic programs

--- Palindrome.java
+++ Palindrome.java
@@ -1,20 +1,21 @@
 package com.company;
-import java.util.Scanner;
+import java.util.Scanner; //for taking user input
 public class Main {
 
     public static void main(String[] args)
     {
         System.out.println("Enter number: ");
         Scanner num=new Scanner(System.in);
-        long n = num.nextLong();
+        long n = num.nextLong(); //using long to initialize to factor in larger numbers
         long temp=n,r,sum=0;
-
+        //looping to extract digits and reverse the number
         while(n>0)
         {
             r=n%10;
             sum=sum*10+r;
             n=n/10;
         }
+        //condition to check if the new reverse number is equal to the initial number
         if(temp==sum)
         {
             System.out.println("Palindrome number");
