68f17b4f65a88628567a13ff96b39383e53dba3b
ombharat6361 <96538649+ombharat6361@users.noreply.github.com>
2021-12-23 19:30:15 +0530

    Java Basic programs

--- Factorial.java
+++ Factorial.java
@@ -5,6 +5,7 @@ import java.util.Scanner; //for taking user input
 public class Main {
 
     public static void main(String[] args) {
+        //The factorial of a number is the product of the number and each preceeding number down to but not including 0.
         System.out.println("Enter number to find the factorial of:");
         Scanner num = new Scanner(System.in);
         long n = num.nextLong();
