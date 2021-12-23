9d2b97c37ba692c66a90b27ac1e3ac004baef036
ombharat6361 <96538649+ombharat6361@users.noreply.github.com>
2021-12-23 21:55:20 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import java.util.Scanner;//for user input
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Enter how long you want the array to be: ");
+        Scanner num= new Scanner(System.in);
+        int n = num.nextInt();
+        int ar[]=new int[n]; //initializing array
+        System.out.println("Enter elements of array: ");
+        for(int i=0;i<n;i++)//looping to take input in array
+        {
+            ar[i]=num.nextInt();
+        }
+        int arc[]=new int[n];
+        for(int j=0;j<n;j++) //looping to copy elements to new array and printing it
+        {
+            arc[j]=ar[j];
+            System.out.println(arc[j]);
+        }
+        System.out.println("Your array has been copied to the new array");
+    }
+}
