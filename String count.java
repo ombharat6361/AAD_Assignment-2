1f75b2b31cc1c95ad6bec759b12d24fd0f967815
ombharat6361 <96538649+ombharat6361@users.noreply.github.com>
2021-12-24 17:13:46 +0530

    Java String programs

--- String count.java
+++ String count.java
@@ -1,14 +1,14 @@
 package com.company;
 
-import java.util.Scanner;
+import java.util.Scanner;//for user input
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Enter the string: ");
         Scanner s=new Scanner(System.in);
-        String str=s.nextLine();
-        int count=0;
-        str=str.replaceAll(" ","");
+        String str=s.nextLine(); //string input; nextLine() inputs all characters that are input
+        int count=0;//starting counter at 0
+        str=str.replaceAll(" ",""); //removing all whitespaces to get accurate results
         for(int i=0;i<str.length();i++)
         {
             count++;
