ea2f75dc5100975c159021375f8c95bfe01b507d
ombharat6361 <96538649+ombharat6361@users.noreply.github.com>
2021-12-24 17:33:33 +0530

    Java String programs

--- Reverse string.java
+++ Reverse string.java
@@ -1,15 +1,16 @@
 package com.company;
 
-import java.util.Locale;
-import java.util.Scanner;
+import java.util.Locale;//for the toLowerCase() function
+import java.util.Scanner;//for user input
 public class Main {
 
     public static void main(String[] args) {
+        //program that reverses a string
         System.out.println("Enter the string: ");
         Scanner s=new Scanner(System.in);
         String str=s.nextLine();
-        str=str.toLowerCase(Locale.ROOT);
-        String ns="";
+        str=str.toLowerCase(Locale.ROOT); //coverting all letters to lowercase
+        String ns=""; //new string which will store the reversed string
         for(int i=str.length()-1;i>=0;i--)
         {
             ns=ns+str.charAt(i);
